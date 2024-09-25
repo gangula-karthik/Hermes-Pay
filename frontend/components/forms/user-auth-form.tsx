dedd578c12ec974a9265528b876f7f7a7961c37e
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2024-09-25 21:01:07 +0800

    add: google auth signup

--- frontend/components/forms/user-auth-form.tsx
+++ frontend/components/forms/user-auth-form.tsx
@@ -16,6 +16,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
 import GithubSignInButton from '../github-auth-button';
+import GoogleSignInButton from '../google-auth-button';
 
 const formSchema = z.object({
   email: z.string().email({ message: 'Enter a valid email address' })
@@ -84,6 +85,7 @@ export default function UserAuthForm() {
         </div>
       </div>
       <GithubSignInButton />
+      <GoogleSignInButton />
     </>
   );
 }
