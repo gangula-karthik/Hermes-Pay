00528cbb96eb3d565db953c3a485bba447912cdc
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2024-10-05 13:16:19 +0800

    update: mock api response

--- backend/mock_app.py
+++ backend/mock_app.py
@@ -14,44 +14,44 @@ def mock_ocr_route():
     try:
         food_items = [
             {
-                "name": "Cheeseburger",
-                "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTENYnRh013tE7LrJCY-j0pTDFoJzJZtAx8qw&s",
-                "price": "$5.99"
+                "name": "nasi ambeng",
+                "image": "https://d3h1lg3ksw6i6b.cloudfront.net/media/image/2020/07/29/09b2cdd7bb324afa8aa82dce584e4900_Hjh+Maimunah+Nasi+Ambeng.jpg",
+                "price": "$5.50"
             },
             {
-                "name": "Pizza",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$7.99"
+                "name": "nasi rawon",
+                "image": "https://angmohintheeast.com/assets/images/bedok-corner-nasi-rawon-1.jpg",
+                "price": "$5.50"
             },
             {
-                "name": "Salad",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$4.99"
+                "name": "nasi lemak",
+                "image": "https://img.freepik.com/premium-photo/ai-generated-illustration-nasi-lemak-malay-fragrant-rice-dish-cooked-coconut-milk_441362-8747.jpg",
+                "price": "$2.50"
             },
             {
-                "name": "Fries",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$2.99"
+                "name": "lontong",
+                "image": "https://media-cdn.tripadvisor.com/media/photo-s/14/0d/cf/e3/lontong-kering.jpg",
+                "price": "$3.00"
             },
             {
-                "name": "Cheeseburger",
-                "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTENYnRh013tE7LrJCY-j0pTDFoJzJZtAx8qw&s",
-                "price": "$5.99"
+                "name": "lontong kering",
+                "image": "https://media-cdn.tripadvisor.com/media/photo-s/14/0d/cf/e3/lontong-kering.jpg",
+                "price": "$3.00"
             },
             {
-                "name": "Pizza",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$7.99"
+                "name": "mee rebus",
+                "image": "https://fullofplants.com/wp-content/uploads/2024/01/vegan-mee-rebus-malaysian-sweet-potato-curry-with-noodles-thumb-1.jpg",
+                "price": "$3.00"
             },
             {
-                "name": "Salad",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$4.99"
+                "name": "mee soto",
+                "image": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgsk3iGNnDtnsZYfRwdaP6Fsk-1jPXxiOOCpmnnHcPDppS4Zr3sz6mP5LNewHr_rcvU_33XLFAhzS4-1N8dpXZB-2-gGnGFEo3Pz5K-AdS5yR3LGqlPPARd8Wv4ygrjfOSDD6ABhADnDitL/s640/IMG_8438.JPG",
+                "price": "$3.00"
             },
             {
-                "name": "Fries",
-                "image": "https://cdn.shopify.com/s/files/1/0274/9503/9079/files/20220211142754-margherita-9920_5a73220e-4a1a-4d33-b38f-26e98e3cd986.jpg?v=1723650067",
-                "price": "$2.99"
+                "name": "soto ayam",
+                "image": "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/23f62312738135ab286e78812093c46b/Derivates/2e373c2ef0bf36c9e8475cbf93347db5a3496309.jpg",
+                "price": "$3.00"
             }
         ]
 
